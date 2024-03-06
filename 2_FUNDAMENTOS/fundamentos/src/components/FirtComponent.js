fefe5c18b4a2ecde1e96991c70c0fcb062077879
Gustavo Dallabrida <gustavodallabridaeduc@gmail.com>
2024-03-05 21:09:23 -0300

    novos exercícios

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirtComponent.js
@@ -0,0 +1,11 @@
+const FirstComponent = () => {
+
+    return (
+        <div>
+            <h1>Meu primeiro componente</h1>
+            <p>Está é a estrutura base.</p>
+        </div>
+    )
+}
+
+export default FirstComponent;
